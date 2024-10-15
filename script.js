a82b8230c6bba5981ff890d8ad211a2dd31b1334
Ryuukz <119050694+Ryuukz@users.noreply.github.com>
2024-10-16 02:46:06 +0700

    script.js

--- script.js
+++ script.js
@@ -16,36 +16,8 @@ window.onscroll = () =>{
     navlist.remove.toggle('active');
 }
 
-var typed1 = new Typed('#text', {
+var typed = new Typed('#text', {
     strings: ['Data Analyst.', 'German Speaker.', 'Machine Learning Enthusiast.'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    loop: true,
-});
-
-var typed2 = new Typed('#text2', {
-    strings: ['1 Month', '2 Months', '3 Months'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    loop: true,
-});
-
-var typed3 = new Typed('#text3', {
-    strings: ['Data Science', 'Applied Life Sciences'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    loop: true,
-});
-
-var typed4 = new Typed('#text4', {
-    strings: ['1', '2', '3', '4', '5'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    loop: true,
-});
-
-var typed5 = new Typed('#text5', {
-    strings: ['Indonesia', 'Germany', 'Singapore'],
     typeSpeed: 100,
     backSpeed: 100,
     loop: true,
